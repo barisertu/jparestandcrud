84368c19326e1a73c730a9f8920bc09e14c03380
barisertu <bertugrul@tsn.at>
2020-05-20 01:00:32 +0200

    commit image

--- src/main/java/com/uebeung3/jparestandcrud/controller/VereinController.java
+++ src/main/java/com/uebeung3/jparestandcrud/controller/VereinController.java
@@ -2,6 +2,7 @@ package com.uebeung3.jparestandcrud.controller;
 
 import com.uebeung3.jparestandcrud.model.Verein;
 import com.uebeung3.jparestandcrud.repository.VereinRepository;
+import com.uebeung3.jparestandcrud.exception.mitgliedNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -31,14 +32,27 @@ public class VereinController {
 
     //get by id
     @GetMapping("/mitglieder/{id}")
-    public ResponseEntity<Verein> getMitgliedById(@PathVariable(value = "id") Long mitgliedId){
-
+    public ResponseEntity<Verein> getMitgliedById(@PathVariable(value = "id") Long mitgliedId)
+            throws mitgliedNotFoundException{
+        Verein mitglied = vereinRepository.findById(mitgliedId)
+                .orElseThrow(() -> new mitgliedNotFoundException("Mitglied mit folgendem ID nicht gefunden: " + mitgliedId));
+        return ResponseEntity.ok().body(mitglied);
     }
 
 
     //update
     @PostMapping("/mitglieder/{id}")
-    public ResponseEntity<Verein> updateMitglied(@PathVariable(value = "id") Long mitgliedId, @RequestBody Verein mitglied){
+    public ResponseEntity<Verein> updateMitglied(@PathVariable(value = "id") Long mitgliedId, @RequestBody Verein mitgliedDetails)
+            throws mitgliedNotFoundException{
+        Verein mitglied = vereinRepository.findById(mitgliedId)
+                .orElseThrow(() -> new mitgliedNotFoundException("Mitglied mit folgendem ID nicht gefunden: " + mitgliedId));
+
+
+        mitglied.setFirstName(mitgliedDetails.getFirstName());
+        mitglied.setLastName(mitgliedDetails.getLastName());
+        mitglied.setBackNumber(mitgliedDetails.getBackNumber());
+        vereinRepository.save(mitglied);
+        return ResponseEntity.ok().body(mitglied);
 
     }
 
@@ -46,8 +60,13 @@ public class VereinController {
 
     //delete
     @DeleteMapping("/mitglieder/{id}")
-    public ResponseEntity<Verein> deleteMitglied(@PathVariable(value = "id") Long mitgliedId, @RequestBody Verein mitglied){
+    public ResponseEntity<Verein> deleteMitglied(@PathVariable(value = "id") Long mitgliedId)
+            throws mitgliedNotFoundException{
+             vereinRepository.findById(mitgliedId)
+                .orElseThrow(() -> new mitgliedNotFoundException("Mitglied mit folgendem ID nicht gefunden: " + mitgliedId));
 
+             vereinRepository.deleteById(mitgliedId);
+             return ResponseEntity.ok().build();
     }
 
 
