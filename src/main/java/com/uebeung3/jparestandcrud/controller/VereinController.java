8ad8dc1cbee91ab39a126b621f1cfdfbd59c8085
barisertu <bertugrul@tsn.at>
2020-05-19 22:35:57 +0200

    erstes Zwischenergebnis

--- src/main/java/com/uebeung3/jparestandcrud/controller/VereinController.java
+++ src/main/java/com/uebeung3/jparestandcrud/controller/VereinController.java
@@ -1,9 +1,13 @@
 package com.uebeung3.jparestandcrud.controller;
 
+import com.uebeung3.jparestandcrud.model.Verein;
 import com.uebeung3.jparestandcrud.repository.VereinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("verein")
@@ -11,5 +15,40 @@ public class VereinController {
 
     @Autowired
     private VereinRepository vereinRepository;
-    
+
+
+    //get all
+    @GetMapping("/mitglieder")
+    public List<Verein> getAllMitglieder(){
+        return vereinRepository.findAll();
+    }
+
+    //create
+    @PostMapping("/mitglieder")
+    public Verein createMitglied(@Validated @RequestBody Verein mitglied){
+      return vereinRepository.save(mitglied);
+    }
+
+    //get by id
+    @GetMapping("/mitglieder/{id}")
+    public ResponseEntity<Verein> getMitgliedById(@PathVariable(value = "id") Long mitgliedId){
+
+    }
+
+
+    //update
+    @PostMapping("/mitglieder/{id}")
+    public ResponseEntity<Verein> updateMitglied(@PathVariable(value = "id") Long mitgliedId, @RequestBody Verein mitglied){
+
+    }
+
+
+
+    //delete
+    @DeleteMapping("/mitglieder/{id}")
+    public ResponseEntity<Verein> deleteMitglied(@PathVariable(value = "id") Long mitgliedId, @RequestBody Verein mitglied){
+
+    }
+
+
 }
